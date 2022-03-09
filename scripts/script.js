25b450772d5f1d883b8eb6ab920e34a4e6f5ce38
Georgy <g.boronoev@gmail.com>
2022-03-09 12:43:28 +0300

    early js

--- scripts/script.js
+++ scripts/script.js
@@ -6,38 +6,9 @@ document.querySelector('.header__item:first-child').addEventListener('click', ()
 })
 
 
-document.querySelectorAll('.currency__to').forEach(element => {
-  element.addEventListener('click', () => {
-    document.querySelectorAll('.currency__to').forEach(element => {
-      element.style.backgroundColor = "white";
-      element.style.borderColor = "#E5E5E5";
-      element.style.color = "#C6C6C6";
-    })
-    element.style.backgroundColor = "#833AE0";
-    element.style.borderColor = "#833AE0";
-    element.style.color = "white";
-    document.querySelectorAll('.option__currency').forEach(option => {
-      option.style.backgroundColor = "white";
-    })
-  })
-})
 
-document.querySelectorAll('.currency__from').forEach(element => {
-  element.addEventListener('click', () => {
-    document.querySelectorAll('.currency__from').forEach(element => {
-      element.style.backgroundColor = "white";
-      element.style.borderColor = "#E5E5E5";
-      element.style.color = "#C6C6C6";
 
-    })
-    element.style.backgroundColor = "#833AE0";
-    element.style.borderColor = "#833AE0";
-    element.style.color = "white";
-    document.querySelectorAll('.option__currency').forEach(option => {
-      option.style.backgroundColor = "white";
-    })
-  })
-})
+
 
 
 document.querySelector('.button__change').addEventListener('click', () => {
@@ -47,30 +18,68 @@ document.querySelector('.button__change').addEventListener('click', () => {
   }, 400)
 })
 
-document.querySelector('.button__change').ondragstart = function() {
+document.querySelector('.button__change').ondragstart = function () {
   return false;
 };
 
 
 
-// if ((document.querySelector("input.input__amount"))){
-// document.querySelector("input.input__amount").addEventListener("input", () => {
-//   console.log(document.querySelector("input.input__amount").value.replace(/^ +| +$|( ) +/g,"$1"))
-//   document.querySelector("input.input__amount").value = new Intl.NumberFormat('ru-RU').format(document.querySelector("input.input__amount").value.replace(/\s/g, ''));
-// })
-// }
+// if ((document.querySelector("input.input__amount").value)) {
+//   document.querySelector("input.input__amount").addEventListener("input", () => {
+//     document.querySelector("input.input__amount").value = new Intl.NumberFormat('ru-RU').format(document.querySelector("input.input__amount").value.replace(/\s/g, ''));
+//   })
+// }//форматирование чисел с добавлением пробелов
 
+let from;
+let to;
+document.querySelectorAll('.currency__from').forEach(element => {if (element.value === '1'){from = element.textContent}})
+document.querySelectorAll('.currency__to').forEach(element => {if (element.value === '1'){to = element.textContent}})
+const url = 'https://api.exchangerate.host/latest';
+fetch(`${url}?base=${from}&symbols=${to}`)
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('.rate-from').textContent = `1 ${from} = ${data.rates[`${to}`].toFixed(4)} ${to}`;
+    document.querySelector('.rate-to').textContent = `1 ${to} = ${(1/data.rates[`${to}`]).toFixed(4)} ${from}`;
+    document.querySelector('.input-to').value = data.rates[`${to}`]
+    document.querySelector("input.input__amount").addEventListener("input", () => {
+      document.querySelector("input.input-to").value = data.rates[`${to}`] * document.querySelector('.input__amount').value;
+    })
+    console.log(data.rates[`${to}`] * 1)
+  })
 
-
-// function deleteSpace(str) {
-//   const returnedArr = []
-//   for(let i = 0; i < str.length; i++){
-//     if(str[i]) {
-//       console.log(str[i])
-//       returnedArr.push(str[i])
-//     } else {console.log(str[i-1], str[i], str[i+1])}
-//   }
-//   const realReturn = returnedArr.filter(symbol => symbol !== ' ')
-//   console.log(realReturn)
-//   return realReturn.join('')
-// }
+  document.querySelectorAll('.currency__to').forEach(element => {
+    element.addEventListener('click', () => {
+      document.querySelectorAll('.currency__to').forEach(element => {
+        element.style.backgroundColor = "white";
+        element.style.borderColor = "#E5E5E5";
+        element.style.color = "#C6C6C6";
+        element.value = '0'
+      })
+      element.value = '1'
+      element.style.backgroundColor = "#833AE0";
+      element.style.borderColor = "#833AE0";
+      element.style.color = "white";
+      document.querySelectorAll('.option__currency').forEach(option => {
+        option.style.backgroundColor = "white";
+      })
+    })
+  })
+  
+  document.querySelectorAll('.currency__from').forEach(element => {
+    element.addEventListener('click', () => {
+      document.querySelectorAll('.currency__from').forEach(element => {
+        element.style.backgroundColor = "white";
+        element.style.borderColor = "#E5E5E5";
+        element.style.color = "#C6C6C6";
+        element.value = '0'
+  
+      })
+      element.value = '1'
+      element.style.backgroundColor = "#833AE0";
+      element.style.borderColor = "#833AE0";
+      element.style.color = "white";
+      document.querySelectorAll('.option__currency').forEach(option => {
+        option.style.backgroundColor = "white";
+      })
+    })
+  })
